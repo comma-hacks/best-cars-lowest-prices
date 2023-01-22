5efdfae599e3891990643510218c35efabd67951
Keyvan Fatehi <keyvanfatehi@gmail.com>
2023-01-21 21:23:38 -0800

    update

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-[ ! -f CARS.md ] || [ $(date -r CARS.md +%s) -lt $(date -d "1 day ago" +%s) ] && wget https://raw.githubusercontent.com/commaai/openpilot/master/docs/CARS.md
+rm -f truecar-offers-* CARS.md
+wget https://raw.githubusercontent.com/commaai/openpilot/master/docs/CARS.md
 [ ! -d mdtable2csv ] && git clone git@github.com:BatuhanKucukali/mdtable2csv.git && pushd mdtable2csv && go build . && popd
 ./mdtable2csv/mdtable2csv convert CARS.md
 cat CARS.md | grep 'Make|Model|' | sed 's/|/,/g' | cut -c2- | head -c -2 > good-cars.csv
@@ -8,3 +9,6 @@ cat CARS.md.csv | grep star-full | grep -v star-empty | grep 'openpilot,0 mph,0 
 node process.js
 rm good-cars.csv
 rm CARS.md.csv
+git add .
+git commit -m "update"
+git push origin master
