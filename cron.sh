80adbc0cd1ea2f734dfbe1fe54a5a2ce93db6fa4
Keyvan Fatehi <keyvanfatehi@gmail.com>
2023-01-28 13:11:19 -0800

    01/28/23 01:11:19 PM

--- cron.sh
+++ cron.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -euxo pipefail
+cd $(dirname $0)
+. $HOME/.asdf/asdf.sh
+if ! hash node; then
+  echo "Node not found"
+  exit 1
+fi
 # Deletes cached data and pushes to github.
 rm -f truecar-offers-* CARS.md
 wget https://raw.githubusercontent.com/commaai/openpilot/master/docs/CARS.md
-./update.sh
+. update.sh
 rm good-cars.csv
 rm CARS.md.csv
 git add .
