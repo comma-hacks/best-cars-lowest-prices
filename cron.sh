e1caac9c4a5b3cdf0c7ec09464eb1458816951cd
Keyvan Fatehi <keyvanfatehi@gmail.com>
2023-02-05 18:25:39 -0800

    allow cron to save historical intermediary

--- cron.sh
+++ cron.sh
@@ -10,8 +10,6 @@ fi
 rm -f truecar-offers-* CARS.md
 wget https://raw.githubusercontent.com/commaai/openpilot/master/docs/CARS.md
 . update.sh
-rm good-cars.csv
-rm CARS.md.csv
 git add .
 git commit -m "$(date +'%D %r')"
 git push origin master
